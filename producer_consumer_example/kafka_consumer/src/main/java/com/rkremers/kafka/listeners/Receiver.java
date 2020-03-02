f919a905cf7afb2bbe0add55ad6678209547e3c8
Rob Kremers <rkremers@bkwi.nl>
2020-03-02 15:07:18 +0100

    Updated readme.txt

--- producer_consumer_example/kafka_consumer/src/main/java/com/rkremers/kafka/listeners/Receiver.java
+++ producer_consumer_example/kafka_consumer/src/main/java/com/rkremers/kafka/listeners/Receiver.java
@@ -1,7 +1,6 @@
 package com.rkremers.kafka.listeners;
 
 import com.rkremers.kafka.entities.Book;
-import com.rkremers.kafka.entities.Todo;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -18,8 +17,5 @@ public class Receiver {
                         @Headers MessageHeaders headers) {
         headers.keySet().forEach(key -> System.out.println(key + ": " + headers.get(key)));
         System.out.println("Received message: " + book.toString());
-
-        Todo todo = new Todo();
-        todo
     }
 }
